a46b6221861081b2a366316b2bf341862c80c693
rrwu23 <rong.rr.wu@gmail.com>
2026-09-15 20:24:07 -0400

    Update API endpoint for adding user

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ form?.addEventListener('submit', async function(event) {
 
   try {
     // 4. Send the request to your server
-    const response = await fetch('https://192.168.4.29:8000/adduser', {
+    const response = await fetch('https://api.rongrongwu.com/adduser', {
       method: 'POST', // Use POST to send data
       body: formData
     });
