c4e34f39e5e9bb3adc6da26a51254d346ec02445
Rongrong Wu <rong.rr.wu@gmail.com>
2026-09-19 12:27:10 -0400

    fix http -> https

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ adminForm?.addEventListener('submit', async function (event) {
     formData.delete('retype_password');
 
     try {
-        const response = await fetch('http://api.rongrongwu.com/addadmin', {
+        const response = await fetch('https://api.rongrongwu.com/addadmin', {
             method: 'POST',
             body: formData
         });
